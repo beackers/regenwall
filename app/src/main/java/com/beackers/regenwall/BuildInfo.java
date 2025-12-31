55eb0b90c2a38ab3e197fd081ce0cdc0cf51a867
Beckett Freeman <71795010+beackers@users.noreply.github.com>
2025-12-31 06:17:15 -0600

    replaced weird symbol
    dunno where ChatGPT got that

--- app/src/main/java/com/beackers/regenwall/BuildInfo.java
+++ app/src/main/java/com/beackers/regenwall/BuildInfo.java
@@ -1,6 +1,6 @@
 package com.beackers.regenwall;
 
 public final class BuildInfo {
-    public static final boolean DEBUG = BuildConfig.DEBUG;
+    public static final boolean DEBUG = true;
     public static final int FlowFieldGeneratorVersion = 1;
 }
