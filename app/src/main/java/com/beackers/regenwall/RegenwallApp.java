48dfb002ac1c3c9e3b669e0a6882c18073fa5425
Beckett Freeman <71795010+beackers@users.noreply.github.com>
2026-01-04 06:10:30 -0600

    add singleton flowfieldconfigstore to application

--- app/src/main/java/com/beackers/regenwall/RegenwallApp.java
+++ app/src/main/java/com/beackers/regenwall/RegenwallApp.java
@@ -4,7 +4,20 @@ import android.app.Application;
 import android.os.StrictMode;
 import android.util.Log;
 
+import androidx.datastore.core.DataStore;
+import androidx.datastore.core.DataStoreFactory;
+import com.beackers.regenwall.datastore.FlowFieldConfigProto;
+import com.beackers.regenwall.datastore.FlowFieldConfigSerializer;
+
+import java.io.File;
+
 public class RegenwallApp extends Application {
+    
+    private static final String DATASTORE_NAME = "flow_field_config.pb";
+
+    private DataStore<FlowFieldConfigProto> flowFieldConfigStore;
+
+
     @Override
     public void onCreate() {
         super.onCreate();
@@ -20,5 +33,14 @@ public class RegenwallApp extends Application {
                     .build());
             Log.d("Regenwall", "Debug mode ON");
         }
+
+        flowFieldConfigStore = DataStoreFactory.create(
+                new FlowFieldConfigSerializer(),
+                () -> new File(getFilesDir(), DATASTORE_NAME)
+                );
+    }
+
+    public DataStore<FlowFieldConfigProto> getFlowFieldConfigStore() {
+        return flowFieldConfigStore;
     }
 }
