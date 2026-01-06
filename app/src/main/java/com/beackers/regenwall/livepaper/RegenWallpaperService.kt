4ddd7337db92ad5a2e2de0b42215fefe83df7fcf
Beckett Freeman <71795010+beackers@users.noreply.github.com>
2026-01-06 11:36:34 -0600

    update kotlin class

--- app/src/main/java/com/beackers/regenwall/livepaper/RegenWallpaperService.kt
+++ app/src/main/java/com/beackers/regenwall/livepaper/RegenWallpaperService.kt
@@ -1,10 +1,21 @@
 package com.beackers.regenwall.livepaper
 
-class RegenwallWallpaperService : WallpaperService() {
-    override fun onCreateEngine() {
+import android.service.wallpaper.WallpaperService
+import android.view.SurfaceHolder
+
+class RegenWallpaperService : WallpaperService() {
+    override fun onCreateEngine(): Engine {
         return RegenEngine()
     }
 
     inner class RegenEngine : Engine() {
+        override fun onSurfaceCreated(holder: SurfaceHolder) {
+            super.onSurfaceCreated(holder)
+            // drawing setup here
+        }
+
+        override fun onSurfaceDestroyed(holder: SurfaceHolder) {
+            super.onSurfaceDestroyed(holder)
+        }
     }
 }
