78ade8e1e60e108a2e206557f240dbbd6530e9c0
beackers <71795010+beackers@users.noreply.github.com>
2026-01-08 14:56:15 -0600

    Implement crash report display in CrashReportActivity
    Added functionality to read and display crash report from a file.

--- app/src/main/java/com/beackers/regenwall/crashcar/CrashReportActivity.java
+++ app/src/main/java/com/beackers/regenwall/crashcar/CrashReportActivity.java
@@ -10,4 +10,24 @@ import android.widget.TextView;
 import java.nio.file.Files;
 
 public class CrashReportActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TextView tv = new TextView(this);
+        tv.setTextSize(12);
+        tv.setPadding(16, 16, 16, 16);
+        tv.setText(readCrash());
+
+        setContentView(tv);
+    }
+
+    private String readCrash() {
+        try {
+            File file = new File(getFilesDir(), "last_crash.txt");
+            return new String(Files.readAllBytes(file.toPath()));
+        } catch (Exception e) {
+            return "Could not read crash report.";
+        }
+    }
 }
