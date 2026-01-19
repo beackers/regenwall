06fd2ea4ba5a58254fc48ed0a545435d43f03ff2
Beckett Freeman <71795010+beackers@users.noreply.github.com>
2026-01-18 18:50:05 -0600

    add crashviewer internals

--- app/src/main/java/com/beackers/regenwall/crashcar/CrashViewerActivity.java
+++ app/src/main/java/com/beackers/regenwall/crashcar/CrashViewerActivity.java
@@ -1,11 +1,49 @@
 package com.beackers.regenwall.crashcar;
 
+import com.beackers.regenwall.R;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Button;
+import android.widget.ScrollView;
+import android.widget.Toast;
+import android.content.ClipboardManager;
+import android.content.ClipData;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 
 public class CrashViewerActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crash_viewer);
+
+        String path = getIntent().getStringExtra("file");
+        File file = new File(path);
+
+        TextView tv = findViewById(R.id.crashText);
+        tv.setText(readFile(file));
+
+        findViewById(R.id.copyButton).setOnClickListener(v -> copy(tv));
+        findViewById(R.id.deleteButton).setOnClickListener(v -> delete(file));
+    }
+
+    private void copy(TextView tv) {
+        ClipboardManager clipboard =
+            (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+        
+        ClipData clip =
+            ClipData.newPlainText("stack trace", tv.getText());
+        clipboard.setPrimaryClip(clip);
+        Toast.makeText(this, "Copied to clipboard", Toast.LENGTH_SHORT);
+    }
+
+    private void delete(File file) {
+        file.delete();
+        finish();
     }
 }
