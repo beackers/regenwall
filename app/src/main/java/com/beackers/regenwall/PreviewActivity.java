347859d37acd77b6f28f9e1e71419cc68ddf46ec
Atticus F. <71795010+beackers@users.noreply.github.com>
2026-01-22 06:41:09 -0600

    fix visibility

--- app/src/main/java/com/beackers/regenwall/PreviewActivity.java
+++ app/src/main/java/com/beackers/regenwall/PreviewActivity.java
@@ -10,7 +10,7 @@ import android.widget.Button;
 import java.io.File;
 import java.io.IOException;
 
-class PreviewActivity extends Activity {
+public class PreviewActivity extends Activity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
